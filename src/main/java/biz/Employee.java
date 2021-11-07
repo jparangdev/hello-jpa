71f84c881e599c7390878e0a28089676b7cea712
jpara <jparangdev@gmail.com>
2021-11-07 22:30:26 +0900

    <chapter2> 127p 까지 진행

--- src/main/java/biz/Employee.java
+++ src/main/java/biz/Employee.java
@@ -2,34 +2,63 @@ package biz;
 
 import java.util.Date;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @Entity
-@Table(name="S_EMP")
+@Table(name = "S_EMP", uniqueConstraints = {@UniqueConstraint(columnNames = {"NAME", "MAILID"})})
+// @NoArgsConstructor
+// @AllArgsConstructor
+@ToString(exclude = {"searchCondition","searchKeyword"})
+@Access(AccessType.FIELD)
 public class Employee {
-	@Id
+	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(length = 7, nullable = false)
 	private long id;
 
+	@Column(length = 25, nullable = false)
 	private String name;
 
+	@Column(length = 8, unique = true)
 	private String mailId;
 
-	@Column(name = "START_DATE")
+	@Column(name = "START_DATE", insertable = true)
+	// @Temporal(TemporalType.DATE)
 	private Date startDate;
 
+	@Column(length = 25)
 	private String title;
 
-	@Column(name="DEPT_NAME")
+	@Column(name = "DEPT_NAME", length = 30)
 	private String deptName;
 
+	@Column(precision = 11, scale = 2)
 	private double salary;
 
-	@Column(name="COMMISSION_PCT")
+	@Column(name = "COMMISSION_PCT", precision = 4, scale = 2,
+		columnDefinition = "double CHECK (commission_pct IN (10, 12.5, 15, 17.5, 20))")
 	private double commissionPct;
+
+	@Transient
+	private String searchCondition;
+
+	@Transient
+	private String searchKeyword;
+
 }
--- src/test/java/biz/EmployeeServiceClient.java
+++ src/test/java/biz/EmployeeServiceClient.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class EmployeeServiceClient {
@@ -14,9 +15,11 @@ public class EmployeeServiceClient {
 
 		EntityManager em = emf.createEntityManager();
 
+		EntityTransaction tx = em.getTransaction();
+
 		try  {
 			Employee employee = new Employee();
-			employee.setId(1);
+			// employee.setId(2L);
 			employee.setName("둘리");
 			employee.setMailId("gurum");
 			employee.setStartDate(new Date());
@@ -25,9 +28,27 @@ public class EmployeeServiceClient {
 			employee.setSalary(2500.00);
 			employee.setCommissionPct(12.50);
 
+			// Employee employee = new Employee(1L, "둘리", "gurum", new Date(), "과장", "총무부"
+			// , 2500.00, 12.50, null, null);
+
+			tx.begin();
+			System.out.println("등록 전 ID: "+employee.getId());
 			em.persist(employee);
+
+			for (int i = 0; i < 30; i++) {
+				 Thread.sleep(1000);
+				System.out.println("ZZZ....");
+			}
+
+			System.out.println("등록 후 ID: "+employee.getId());
+			tx.commit();
+
+			// Employee findEmployee = em.find(Employee.class, 1L);
+			// System.out.println("검색한 회원정보");
+			// System.out.println(findEmployee.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
+			tx.rollback();
 		} finally {
 			em.close();
 			emf.close();
