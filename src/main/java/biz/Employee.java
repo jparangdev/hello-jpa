ccf391fb82b1e6c88fa40cbdedad434530ecfa65
jparangdev <jparangdev@gmail.com>
2021-11-18 09:53:45 +0900

    챕터 4 완료

--- src/main/java/biz/Employee.java
+++ src/main/java/biz/Employee.java
@@ -1,20 +1,19 @@
 package biz;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Entity
 @Table(name = "S_EMP")
+@ToString(exclude = "employeeCard")
 public class Employee {
 
 	@Id
@@ -24,13 +23,4 @@ public class Employee {
 	@Column(length = 25, nullable = false)
 	private String name;
 
-	@ManyToOne(cascade = CascadeType.PERSIST)
-	@JoinColumn(name = "DEPT_ID")
-	private Department department;
-
-	public void setDepartment(Department department) {
-		this.department = department;
-		department.getEmployeeList().add(this);
-	}
-	
 }
--- src/main/java/biz/EmployeeCard.java
+++ src/main/java/biz/EmployeeCard.java
@@ -1,32 +1,31 @@
 package biz;
 
-import static javax.persistence.FetchType.*;
-
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
+@Table(name = "S_EMP_CARD")
 public class EmployeeCard {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long cardId;
 
 	private LocalDateTime expireDate;
 
 	private String role;
 
-	@OneToOne(fetch = LAZY)
+	@MapsId
+	@OneToOne
 	@JoinColumn(name = "Id")
-	@Column(name = "EMP_CARD_ID")
 	private Employee employee;
 }
