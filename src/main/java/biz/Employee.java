99a49a09e1bd99dfd35a03fa0e0a10fe400027b2
jparangdev <jparangdev@gmail.com>
2021-11-22 11:05:22 +0900

    챕터 6 jpql 완

--- src/main/java/biz/Employee.java
+++ src/main/java/biz/Employee.java
@@ -5,8 +5,9 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Builder;
@@ -17,12 +18,12 @@ import lombok.ToString;
 @Data
 @Entity
 @Table(name = "S_EMP")
-@ToString(exclude = "employeeCard")
+@ToString(exclude = "dept")
 @NoArgsConstructor
 public class Employee {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	private Long id;
 
 	@Column(length = 25, nullable = false)
@@ -34,21 +35,29 @@ public class Employee {
 
 	private String title;
 
-	private String deptName;
+	@ManyToOne
+	@JoinColumn(name = "DEPT_ID")
+	private Department dept;
 
 	private Double salary;
 
 	private Double commissionPct;
 
+	public void setDept(Department department) {
+		this.dept = department;
+		if (department != null)
+			department.getEmployeeList().add(this);
+	}
+
 	@Builder
-	public Employee(Long id, String name, String mailId, LocalDate startDate, String title, String deptName,
+	public Employee(Long id, String name, String mailId, LocalDate startDate, String title, Department dept,
 		Double salary, Double commissionPct) {
 		this.id = id;
 		this.name = name;
 		this.mailId = mailId;
 		this.startDate = startDate;
 		this.title = title;
-		this.deptName = deptName;
+		this.dept = dept;
 		this.salary = salary;
 		this.commissionPct = commissionPct;
 	}
--- src/test/java/biz/JpqlBasicClient.java
+++ src/test/java/biz/JpqlBasicClient.java
@@ -2,7 +2,6 @@ package biz;
 
 import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -35,14 +34,23 @@ public class JpqlBasicClient {
 		// System.out.println("검색된 직업목록");
 		// employeeList.forEach(System.out::println);
 
-		String jpql = "SELECT id, name, deptName, salary FROM Employee";
-		Query query = em.createQuery(jpql);
-		List<Object[]> list = query.getResultList();
+		// String jpql = "SELECT id, name, deptName, salary FROM Employee";
+		// Query query = em.createQuery(jpql);
+		// List<Object[]> list = query.getResultList();
 
-		System.out.println("검색된 직업목록");
-		for (Object[] objects : list) {
-			System.out.println(Arrays.toString(objects));
-		}
+		String jpql = "SELECT id, name, title, salary FROM Employee WHERE id = ?1 AND name = ?2";
+		Query query = em.createQuery(jpql);
+		query.setParameter(1, 1L);
+		query.setParameter(2, "직원1");
+
+		Object[] list = (Object[])query.getSingleResult();
+		System.out.println(list[0] + "번의 정보");
+		System.out.println(Arrays.toString(list));
+
+		// System.out.println("검색된 직업목록");
+		// for (Object[] objects : list) {
+		// 	System.out.println(Arrays.toString(objects));
+		// }
 
 		em.close();
 	}
@@ -51,17 +59,18 @@ public class JpqlBasicClient {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 
-		for (int i = 0; i < 15; i++) {
+		for (int i = 1; i <= 15; i++) {
 			Employee employee = Employee.builder()
 				.name("직원" + i)
 				.mailId("anti-corona" + i)
-				.deptName("개발부")
+				// .deptName("개발부")
 				.salary(12700.00 * i)
 				.startDate(LocalDate.now())
 				.title("사원")
 				.commissionPct(15.00)
 				.build();
 			em.persist(employee);
+			// System.out.println(employee.toString());
 		}
 
 		em.getTransaction().commit();
